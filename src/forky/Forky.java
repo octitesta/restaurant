c17f0c58c7b824476cff2c8ac58035752df4eb80
licholucero <licholucero7@gmail.com>
2019-08-06 11:47:16 -0300

    pedidos

--- src/forky/Forky.java
+++ src/forky/Forky.java
@@ -1,22 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package forky;
 
-/**
- *
- * @author octa
- */
 public class Forky {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-//holoa
+      Pedidos pd = new Pedidos();
+      pd.setVisible(true);
     }
     
 }
